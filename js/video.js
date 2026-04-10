38a8424e78853161f2987f559557246f2ba9b851
EmmanuelAchie <emmanach@umich.edu>
2026-04-09 20:32:02 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,58 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+video = document.querySelector("#player1")
+video.autoplay = false;
+video.loop = false;
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate *= 0.9;
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate /= 0.9;
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 10 >= video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 10;
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false) {
+		video.muted = true;
+		this.textContent = "Unmute";
+	}
+	else {
+		video.muted = false;
+		this.textContent = "Mute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value / 100;
+	document.querySelector("#volume").textContent = this.value + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
